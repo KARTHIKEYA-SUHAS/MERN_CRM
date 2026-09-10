bdb21893c7a5f2a6e111e330664e27442e701ff5
MSK-Crita <msuhas@crita.in>
2026-09-10 19:49:20 +0530

    Updated the deploy error

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import { User } from "../models/User.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError } from "../utils/apiError.js";
+import { ApiError } from "../utils/ApiError.js";
 import { generateToken } from "../utils/generateToken.js";
 
 const toclientUser = (user) => ({
